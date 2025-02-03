03a717e39e9d052107b40b54c3b50db4dc2b86fe
Gabriel Santino <ggs2209@gmail.com>
2025-02-03 16:34:02 -0300

    Projeto Criado

---
+++ DesafioBackEnd/src/test/java/com/bradesco/DesafioBackEnd/DesafioBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bradesco.DesafioBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
